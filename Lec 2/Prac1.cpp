9ad105e69e8d05276f1a4d304b4e1cc547de2d05
Anmol Girase <133815273+techannu872@users.noreply.github.com>
2024-01-16 12:25:50 +0530

    Diving into world of cpp

---
+++ Lec 2/Prac1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 123 ;
+    cout<<"The value of a is :"<<a<<endl;
+
+    float b = 12.4;
+    cout<<"The value of b is:"<<b<<endl;
+
+    double c = 13.444;
+    cout<<"The value of c is :"<<c<<endl;
+
+    char d = 'a';
+    cout<<"The value of d is :"<<d<<endl;
+
+    bool e = true;
+    cout<<"The value of e is :"<<d<<endl;
+
+    int size = sizeof(e);
+    cout<<"The value of size is :"<<size<<endl;
+
+
+}
