252d1eac32415afcd6b2629a032d1868af0c1a92
Anmol Girase <133815273+techannu872@users.noreply.github.com>
2024-03-20 07:47:38 +0530

    Add files via upload

---
+++ Prac2.cpp
@@ -0,0 +1,26 @@
+//Practising Operators
+//arithmetic 
+//relational
+//logical
+//assignment operator
+#include<iostream>
+using namespace std;
+
+int main (){
+    int a = 12;
+    float b = 34;
+
+    int sum = a + b;
+    cout<<sum<<endl;
+
+    int diff = a - b;
+    cout<<diff<<endl;
+    
+    int product = a * b;
+    cout<<product<<endl;
+    
+    float div = b / a;
+    cout<<div<<endl;
+    
+
+}
