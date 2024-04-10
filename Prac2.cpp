8dadf26338ad15d699a8467cafb9ba25d8441f4b
Anmol Girase <133815273+techannu872@users.noreply.github.com>
2024-04-10 22:03:19 +0530

    Add files via upload

--- Prac2.cpp
+++ Prac2.cpp
@@ -1,26 +1,21 @@
-//Practising Operators
-//arithmetic 
-//relational
-//logical
-//assignment operator
+//To determine whether the input no. is positive or negative
 #include<iostream>
 using namespace std;
 
 int main (){
-    int a = 12;
-    float b = 34;
+    float a; 
+    cout<<"Bro apne hisab se number daalo : ";
+    cin>>a;
 
-    int sum = a + b;
-    cout<<sum<<endl;
+    if(a>0){
+        cout<<"A zero se bada hai isliye A positive Hai"<<endl;
+    }
+    if(a<0){
+        cout<<"A zero se kam hone ke karan woh negative hai"<<endl;
+    }
+    else{
+        cout<<"A zero ke barabar isliye woh na toh negative hai aur nahi positive hai"<<endl;
+    }
 
-    int diff = a - b;
-    cout<<diff<<endl;
-    
-    int product = a * b;
-    cout<<product<<endl;
-    
-    float div = b / a;
-    cout<<div<<endl;
-    
 
 }
